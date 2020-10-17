1034975f7d70edf32191b2ad2ad34bcc2fa14217
huangzhihao778 <71325042+huangzhihao778@users.noreply.github.com>
2020-10-17 10:41:38 +0800

    Create 汉诺塔问题.py

---
+++ 汉诺塔问题.py
@@ -0,0 +1,8 @@
+def moveTower(height,fromPole,withPole,toPole):
+    if height>=1:
+        moveTower(height-1,fromPole,toPole,withPole)
+        moveDisk(height,fromPole,toPole)
+        moveTower(height-1,withPole,fromPole,toPole)
+def moveDisk(disk,fromPole,toPole):
+    print(f"moving disk[{disk}] from {fromPole} to {toPole}")
+moveTower(6,"#1","#2","#3")
